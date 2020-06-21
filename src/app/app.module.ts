2eee8067eef799c8facaa8a2012a25420778784b
edithntinyari <emothegi@gmail.com>
2020-06-21 12:50:14 +0300

    added dashboardtest.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardtestComponent } from './dashboardtest/dashboardtest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    DashboardtestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboardtest/dashboardtest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardtest',
+  templateUrl: './dashboardtest.component.html',
+  styleUrls: ['./dashboardtest.component.css']
+})
+export class DashboardtestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
